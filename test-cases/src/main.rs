782857d4a4e0de7b60b143065364b32b05e30759
tvallotton <tvallotton@uc.cl>
2022-07-05 12:55:01 -0400

    feat: appears to be ready

--- test-cases/src/main.rs
+++ test-cases/src/main.rs
@@ -3,11 +3,30 @@ use dioxus::prelude::*;
 
 fn foo() {
     
-    let x = 10; 
-    rsx! {div {
-        "number: {x}"
-    }};
-    html!(<div>"number: {x}"</div>); 
+    let color = "red"; 
+
+    rsx! {
+        div {
+            h1 {
+                "Lorem ipsum dolor sit"
+            }
+            p {
+                "amet consectetur adipisicing elit."
+            }
+            img {
+                src: "https://example.com", 
+                alt: "Facilis harum sequi", 
+            }
+        }
+    }; 
+    html!(
+        <div>
+        <h1>"Lorem ipsum dolor sit"</h1> 
+            <p>"amet consectetur adipisicing elit."</p>
+            <img src="https://example.com" alt="Facilis harum sequi"/>
+        </div>
+    ); 
+    
 }
 
 fn main() {}
