a10fd2ca171e98518ca6d9997aa242a281927cef
tvallotton <tvallotton@uc.cl>
2022-07-06 01:15:00 -0400

    doc: fix top doc comments

--- README.md
+++ README.md
@@ -1,19 +1,14 @@
 
 # dioxus html macro
 This crate offers an `html!` like macro for 
-dioxus applications. It expands to the equivalent `rsx!` macro
-call you would have made otherwise, so it does not rely on any 
-dioxus' internals. 
+dioxus applications. This macro uses `rsx!` internally.
 ```rust
-use dioxus::prelude::*; 
-use dioxus_html_macro::html; 
-
 fn app(cx: Scope) -> Element {
     let mut count = use_state(&cx, || 0);
     cx.render(html!(
         <h1>"High-Five counter: {count}"</h1>
-        <button onclick={move |_| count += 1}>"Up high!"</button>
-        <button onclick={move |_| count -= 1}>"Down low!"</button>
+        <button onclick={ move |_| count += 1 }>"Up high!"</button>
+        <button onclick={ move |_| count -= 1 }>"Down low!"</button>
     ))
 }
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,27 @@
-/// # dioxus html macro
-/// This crate offers an `html!` like macro for
-/// dioxus applications. It expands to the equivalent `rsx!` macro
-/// call you would have made otherwise, so it does not rely on any
-/// dioxus' internals.
-/// ```rust
-/// # use dioxus::prelude::*;
-/// # use dioxus_html_macro::html;
-/// fn app(cx: Scope) -> Element {
-///     let mut count = use_state(&cx, || 0);
-///     cx.render(html!(
-///         <h1>"High-Five counter: {count}"</h1>
-///         <button onclick={move |_| count += 1}>"Up high!"</button>
-///         <button onclick={move |_| count -= 1}>"Down low!"</button>
-///     ))
-/// }
-/// ```
-/// Note that unlike HTML and JSX, styling of html tags is done via
-/// attributes:
-/// ```rust
-/// # use dioxus::prelude::*;
-/// # use dioxus_html_macro::html;
-/// html!(
-///     <h1 color="red">"Title"</h1>
-/// );
-/// ```
+//! # dioxus html macro
+//! This crate offers an `html!` like macro for
+//! dioxus applications. This macro uses `rsx!` internally.
+//! ```rust
+//! # use dioxus::prelude::*;
+//! # use dioxus_html_macro::html;
+//! fn app(cx: Scope) -> Element {
+//!     let mut count = use_state(&cx, || 0);
+//!     cx.render(html!(
+//!         <h1>"High-Five counter: {count}"</h1>
+//!         <button onclick={ move |_| count += 1 }>"Up high!"</button>
+//!         <button onclick={ move |_| count -= 1 }>"Down low!"</button>
+//!     ))
+//! }
+//! ```
+//! Note that unlike HTML and JSX, styling of html tags is done via
+//! attributes:
+//! ```rust
+//! # use dioxus::prelude::*;
+//! # use dioxus_html_macro::html;
+//! html!(
+//!     <h1 color="red">"Title"</h1>
+//! );
+//! ```
 
 #[macro_use]
 extern crate syn;
